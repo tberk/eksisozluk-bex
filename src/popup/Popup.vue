fb04d30123b149b9643b88430f794540bf8858bc
Tuğberk Kılıç <kilictugberk@gmail.com>
2023-05-12 10:23:23 +0300

    refactor: update popup see entries

--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { storageDemo } from '~/logic/storage'
+import { entries } from '~/logic/entries'
 
 function openOptionsPage() {
   browser.runtime.openOptionsPage()
@@ -8,15 +9,20 @@ function openOptionsPage() {
 
 <template>
   <main class="w-[300px] px-4 py-5 text-center text-gray-700">
-    <Logo />
-    <div>Popup</div>
-    <SharedSubtitle />
+    <img src="/assets/icon-512.png" class="h-12 mx-auto mb-1">
+    <div>ekşi sözlük entry silici</div>
+    <div>
+      list status vs
+    </div>
 
     <button class="btn mt-2" @click="openOptionsPage">
       Open Options
     </button>
     <div class="mt-2">
       <span class="opacity-50">Storage:</span> {{ storageDemo }}
+    </div>
+    <div>
+      entries: {{ entries }}
     </div>
   </main>
 </template>
