df7ca55fd6ad3fd285bc5d7d30ba23ef37d953c6
Tuğberk Kılıç <kilictugberk@gmail.com>
2023-05-12 10:22:55 +0300

    feat: add menu items to remove entries

--- src/contentScripts/index.ts
+++ src/contentScripts/index.ts
@@ -3,6 +3,7 @@ import { onMessage } from 'webext-bridge/content-script'
 import { createApp } from 'vue'
 import App from './views/App.vue'
 import { setupApp } from '~/logic/common-setup'
+import { entries } from '~/logic/entries'
 
 // Firefox `browser.tabs.executeScript()` requires scripts return a primitive value
 (() => {
@@ -12,6 +13,13 @@ import { setupApp } from '~/logic/common-setup'
   onMessage('tab-prev', ({ data }) => {
     console.log(`[vitesse-webext] Navigate from page "${data.title}"`)
   })
+
+  //
+  injectEk$i()
+
+  window.onload = function () {
+    setTimeout(createSubMenuItems, 1000)
+  }
 
   // mount component to context window
   const container = document.createElement('div')
@@ -28,3 +36,85 @@ import { setupApp } from '~/logic/common-setup'
   setupApp(app)
   app.mount(root)
 })()
+
+function injectEk$i() {
+  const th = document.getElementsByTagName('body')[0]
+  const s = document.createElement('script')
+  s.setAttribute('type', 'text/javascript')
+  s.setAttribute('src', browser.runtime.getURL('assets/eksiScript.js'))
+  th.appendChild(s)
+}
+
+function showMsg(msg: string) {
+  window.postMessage({ type: 'FROM_CONTENT_SCRIPT', text: msg }, '*')
+}
+
+function createSubMenuItems() {
+  const uls = document.querySelectorAll('ul.dropdown-menu.right.toggles-menu')
+
+  console.log(uls.length)
+  for (let i = 0; i < uls.length; i++) {
+    const ul = uls[i] as HTMLUListElement
+    const id = getEntryIdFromUL(ul)
+
+    if (id === null)
+      continue
+
+    // Create custom menu item
+    const label = 'silme listesine ekle'
+
+    // Added before?
+    if (ul.innerText.search(label) !== -1)
+      continue
+
+    const a = document.createElement('a')
+    a.classList.add('add-entry-to-delete-list')
+    a.innerText = label
+    a.style.fontWeight = 'bold'
+    const li = document.createElement('li')
+    li.appendChild(a)
+    uls[i].appendChild(li)
+
+    // Add click listener
+    li.addEventListener('click', () => {
+      addEntryToList(id)
+    })
+  }
+}
+
+// Add entry to list to be deleted
+function addEntryToList(id: number) {
+  // Hide menu
+  document.body.click()
+
+  // Check if the id already exists in the list
+  if (entries.value.includes(id)) {
+    showMsg('entry silme sırasında.')
+
+    return // Exit the function if the id already exists
+  }
+
+  // If the id doesn't exist, push it to the list
+  entries.value.push(id)
+
+  // Success
+  showMsg('silme listesine eklendi.')
+}
+
+// Gets entry id from ul menu
+function getEntryIdFromUL(ul: HTMLUListElement): number | null {
+  const links = ul.getElementsByTagName('a')
+
+  for (let i = 0, max = links.length; i < max; i++) {
+    const link = links[i]
+
+    if (link.href.includes('duzelt')) {
+      const parts = link.href.split('/')
+
+      if (parts.length > 1)
+        return parseInt(parts[parts.length - 1])
+    }
+  }
+
+  return null
+}
