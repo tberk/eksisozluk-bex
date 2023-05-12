84c04f9ee2e970ec5538a75ad2ad6c3aef87bec9
Tuğberk Kılıç <kilictugberk@gmail.com>
2023-05-12 16:29:51 +0300

    feat: add entry deletion to content hook

--- extension/assets/eksiScript.js
+++ extension/assets/eksiScript.js
@@ -3,7 +3,25 @@ window.addEventListener('message', (event) => {
   if (event.source !== window)
     return
 
+  // Shows eksi ui messages
   if (event.data.type && (event.data.type === 'FROM_CONTENT_SCRIPT'))
     // eslint-disable-next-line no-undef
     ek$i.success(event.data.text)
+
+  // Makes delete calls
+  if (event.data.type && (event.data.type === 'FROM_CONTENT_SCRIPT_DELETE')) {
+    // Log
+    // eslint-disable-next-line no-console
+    console.log(`Making a delete call to: ${event.data.id}`)
+
+    // Delete
+    // eslint-disable-next-line no-undef
+    $.post('/entry/sil', { id: event.data.id })
+      .done(() => {
+        window.postMessage({ type: 'ENTRY_DELETE_SUCCESS', id: event.data.id }, '*')
+      })
+      .fail(() => {
+        window.postMessage({ type: 'ENTRY_DELETE_FAIL', id: event.data.id }, '*')
+      })
+  }
 }, false)
