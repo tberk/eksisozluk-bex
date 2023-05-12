1f0c0015cf7c580c95f6b744edf33e92bbe23235
Tuğberk Kılıç <kilictugberk@gmail.com>
2023-05-12 16:23:23 +0300

    refactor: add helper methods to entry list

--- src/logic/entries.ts
+++ src/logic/entries.ts
@@ -1,5 +1,19 @@
+import { storage } from 'webextension-polyfill'
 import { useStorageLocal } from '~/composables/useStorageLocal'
 
 const arr: number[] = []
 
-export const entries = useStorageLocal('entries', arr)
+export const entries = useStorageLocal('entries', JSON.stringify(arr))
+
+export const getList = async () => {
+  const list = (await storage.local.get('entries')).entries
+
+  return JSON.parse(list)
+}
+
+export const saveList = async (arr: Array) => {
+  entries.value = JSON.stringify(arr)
+}
+
+// Test
+// console.log(Array.isArray(entries.value))
