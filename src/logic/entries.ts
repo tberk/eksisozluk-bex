8c5d4d3491a9b762004b399becbcde8e3c53bca8
Tuğberk Kılıç <kilictugberk@gmail.com>
2023-05-13 19:23:49 +0300

    feat: keep deleted entries record

--- src/logic/entries.ts
+++ src/logic/entries.ts
@@ -3,6 +3,7 @@ import { useStorageLocal } from '~/composables/useStorageLocal'
 
 const arr: number[] = []
 
+// array for entries to be deleted
 export const entries = useStorageLocal('entries', JSON.stringify(arr))
 
 export const getList = async (): Promise<number[]> => {
@@ -21,6 +22,41 @@ export const removeEntry = async (id: number) => {
   list = list.filter(item => item !== id)
 
   saveList(list)
+}
+
+export const addEntry = async (id: number) => {
+  const list = await getList()
+
+  if (!id || list.includes(id))
+    return
+
+  list.push(id)
+
+  saveList(list)
+}
+
+// array for entries that are deleted
+export const deletedEntries = useStorageLocal('deletedEntries', JSON.stringify(arr))
+
+export const getDeletedEntries = async (): Promise<number[]> => {
+  const list = (await storage.local.get('deletedEntries')).deletedEntries
+
+  return JSON.parse(list)
+}
+
+export const saveDeletedEntries = (arr: number[]) => {
+  deletedEntries.value = JSON.stringify(arr)
+}
+
+export const addDeletedEntry = async (id: number) => {
+  const list = await getDeletedEntries()
+
+  if (!id || list.includes(id))
+    return
+
+  list.push(id)
+
+  saveDeletedEntries(list)
 }
 
 // Test
