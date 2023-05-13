dc10a005cf030b40dfdb0f883554aae5202acb8c
Tuğberk Kılıç <kilictugberk@gmail.com>
2023-05-13 19:24:23 +0300

    feat: show deleted entries in popup, improve ui

--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
-import { useIntervalFn } from '@vueuse/core'
-import { getList, saveList } from '~/logic/entries'
+import { useIntervalFn, useToggle } from '@vueuse/core'
+import { getDeletedEntries, getList, saveList } from '~/logic/entries'
 
 const list: Ref<number[]> = ref([])
+const listDeleted: Ref<number[]> = ref([])
+const [showDeletedList, toggleDeletedList] = useToggle()
+const [showList, toggleList] = useToggle()
 
 onMounted(async () => {
   syncList()
@@ -13,6 +16,7 @@ onMounted(async () => {
 
 async function syncList() {
   list.value = await getList()
+  listDeleted.value = await getDeletedEntries()
 }
 
 function clearList() {
@@ -31,18 +35,35 @@ function clearList() {
       ekşi sözlük entry silici
     </div>
     <div class="mt-1 text-gray-400">
-      {{ list.length === 0 ? 'liste boş' : `${list.length} entry silme listesinde` }}
+      <div v-if="list.length === 0">
+        liste boş
+      </div>
+      <button v-else @click="toggleList()">
+        {{ `${list.length} entry silme listesinde` }}
+      </button>
     </div>
 
-    <template v-if="list.length > 0">
-      <button class="btn btn-warning mt-2 flex items-center gap-2 mx-auto" @click="clearList">
-        <mdi:delete-sweep-outline /> listeyi temizle
-      </button>
-
-      <div class="grid grid-cols-3 max-h-[120px] overflow-y-auto mt-2">
+    <template v-if="list.length > 0 && showList">
+      <div class="grid grid-cols-3 max-h-[120px] overflow-y-auto mt-2 bg-green-50 rounded">
         <div v-for="e in list" :key="e" class="hover:bg-gray-100 p-1 rounded">
           <a :href="`https://eksisozluk2023.com/entry/${e}`" target="_blank">#{{ e }}</a>
         </div>
+      </div>
+
+      <button class="btn mt-2 flex items-center gap-2 mx-auto" @click="clearList">
+        <mdi:delete-sweep-outline /> listeyi temizle
+      </button>
+    </template>
+
+    <template v-if="listDeleted.length > 0">
+      <div class="mt-1 text-red-400 cursor-pointer" @click="toggleDeletedList()">
+        şimdiye kadar {{ listDeleted.length }} entry silindi.
+      </div>
+
+      <div v-if="showDeletedList" class="grid grid-cols-3 max-h-[120px] overflow-y-auto mt-2 bg-red-100 rounded">
+        <a v-for="e in listDeleted" :key="e" :href="`https://eksisozluk2023.com/entry/${e}`" target="_blank" class="hover:bg-gray-100 py-1 px-1 rounded">
+          #{{ e }}
+        </a>
       </div>
     </template>
 
