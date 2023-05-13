006566512d2fd4f8b5d689f4f926ada003e07860
Tuğberk Kılıç <kilictugberk@gmail.com>
2023-05-13 17:55:22 +0300

    feat: add remove entry to entries

--- src/logic/entries.ts
+++ src/logic/entries.ts
@@ -5,14 +5,22 @@ const arr: number[] = []
 
 export const entries = useStorageLocal('entries', JSON.stringify(arr))
 
-export const getList = async () => {
+export const getList = async (): Promise<number[]> => {
   const list = (await storage.local.get('entries')).entries
 
   return JSON.parse(list)
 }
 
-export const saveList = async (arr: Array) => {
+export const saveList = (arr: number[]) => {
   entries.value = JSON.stringify(arr)
+}
+
+export const removeEntry = async (id: number) => {
+  let list = await getList()
+
+  list = list.filter(item => item !== id)
+
+  saveList(list)
 }
 
 // Test
