705e0b34513737401cc4bd84b8fba335947bd86e
Tuğberk Kılıç <kilictugberk@gmail.com>
2023-05-13 19:26:37 +0300

    refactor: change ui event names

--- extension/assets/eksiScript.js
+++ extension/assets/eksiScript.js
@@ -1,18 +1,20 @@
 window.addEventListener('message', (event) => {
   // We only accept messages from ourselves
-  if (event.source !== window)
+  if (event.source !== window || !event.data.type)
     return
 
   // Shows eksi ui messages
-  if (event.data.type && (event.data.type === 'FROM_CONTENT_SCRIPT'))
+  if (event.data.type === 'FROM_CONTENT_SCRIPT_UI_MESSAGE')
     // eslint-disable-next-line no-undef
     ek$i.success(event.data.text)
 
   // Makes delete calls
-  if (event.data.type && (event.data.type === 'FROM_CONTENT_SCRIPT_DELETE')) {
+  if (event.data.type === 'FROM_CONTENT_SCRIPT_DELETE') {
     // Log
     // eslint-disable-next-line no-console
     console.log(`Making a delete call to: ${event.data.id}`)
+
+    window.postMessage({ type: 'ENTRY_DELETE_SUCCESS', id: event.data.id }, '*')
 
     // Delete
     // eslint-disable-next-line no-undef
